7eea7650134ebc1aa74760000e11176e10bca100
Alexandre Le Goff <legoff.alexandre06@gmail.com>
2019-04-08 18:35:53 -0700

    header file in includes

---
+++ includes/grimly.h
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   grimly.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ale-goff <ale-goff@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/22 12:45:25 by ale-goff          #+#    #+#             */
+/*   Updated: 2018/10/23 21:15:41 by ale-goff         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GRIMLY_H
+# define GRIMLY_H
+# include <stdlib.h>
+# include <libft.h>
+# include <fcntl.h>
+# include <limits.h>
+
+int					ft_printf(const char *fmt, ...);
+typedef struct		s_map
+{
+	int				x;
+	int				y;
+	char			full;
+	char			path;
+	char			entry;
+	char			exit;
+	char			empty;
+	char			**map;
+	char			*line;
+}					t_map;
+
+typedef struct		s_point
+{
+	int				x;
+	int				y;
+	struct s_point	*papa;
+	struct s_point	*next;
+}					t_point;
+
+typedef struct		s_queue
+{
+	t_point			*begin;
+	t_point			*end;
+}					t_queue;
+
+typedef struct		s_coord
+{
+	int				x_entry;
+	int				y_entry;
+	int				x_exit;
+	int				y_exit;
+}					t_coord;
+
+t_map				*init_map(void);
+t_coord				*init_coord(void);
+int					is_empty(t_queue *queue);
+void				push_queue(t_queue *queue, t_point *pt, int x, int y);
+t_point				*pop_queue(t_queue *queue);
+int					parse_line(t_map *map, int fd, t_coord *coord, t_point **point);
+t_point				*init_point(t_coord *cord);
+int					bfs(t_map *map, t_point *src);
+
+#endif
